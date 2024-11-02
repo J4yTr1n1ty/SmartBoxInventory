10ea22385d15c137967f5b4be9eb8a2cbf297e04
lucyna <support@kushinada.ch>
2024-11-02 22:14:52 +0100

    shirry aim, mby

---
+++ code/src/app/root/pages/box-detiail/box-detiail.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root_box-detail-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-detiail.page.html',
+  styleUrl: './box-detiail.page.scss'
+})
+export class BoxDetiailPage {
+
+}
