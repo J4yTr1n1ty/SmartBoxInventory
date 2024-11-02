66be644b9a205aa6175257d412d32526b825437f
Vali <valentinbaumann.fh@gmail.com>
2024-11-02 15:33:50 +0100

    basic add item logic

--- code/src/app/root/pages/vali-test/vali-test.page.ts
+++ code/src/app/root/pages/vali-test/vali-test.page.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { StateService } from '@root/state/state.service';
+import { BoxModel } from '@root/shared/models/box.model';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'root_vali-test-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './vali-test.page.html',
   styleUrl: './vali-test.page.scss',
 })
-export class ValiTestPage {}
+
+export class ValiTestPage {
+  boxes: Signal<BoxModel[]>;
+
+  constructor(private stateService: StateService){
+    this.boxes = stateService.getAllBoxes()
+  }
+}
+
+
