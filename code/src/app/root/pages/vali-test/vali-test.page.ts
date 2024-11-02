1a5f7e39b0e5792ddde28ec36eaeb91fc5c518d6
Vali <valentinbaumann.fh@gmail.com>
2024-11-02 17:08:12 +0100

    cleanup item component

--- code/src/app/root/pages/vali-test/vali-test.page.ts
+++ code/src/app/root/pages/vali-test/vali-test.page.ts
@@ -1,34 +1,19 @@
-import { Component, Signal } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { StateService } from '@root/state/state.service';
-import { BoxModel } from '@root/shared/models/box.model';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ItemComponent } from '@root/components/item/item.component';
+
 
 @Component({
   selector: 'root_vali-test-page',
   standalone: true,
   imports: [
-    CommonModule,
-    MatSelectModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatCardModule
+ItemComponent
   ],
   templateUrl: './vali-test.page.html',
   styleUrl: './vali-test.page.scss',
 })
 
 export class ValiTestPage {
-  boxes: Signal<BoxModel[]>;
 
-  constructor(private stateService: StateService){
-    this.boxes = stateService.getAllBoxes()
-  }
 }
 
 
