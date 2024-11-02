970ba58be54b92364b9af947f884b3ea255a454a
Jay <jay@projectchaos.ch>
2024-11-02 21:00:15 +0100

    added working footer - TODO: Center icon inside button

--- code/src/app/root/components/footer/footer.component.ts
+++ code/src/app/root/components/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
