3163a428446b3d993cb85d36841d0ec0e4e5407f
Vali <valentinbaumann.fh@gmail.com>
2024-11-03 00:07:57 +0100

    Homepage

--- code/src/app/root/components/home/home.component.ts
+++ code/src/app/root/components/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'root_home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isHidden: boolean = true
 
+  showGuide(): void {
+    this.isHidden = false
+    console.log(this.isHidden)
+  }
 }
