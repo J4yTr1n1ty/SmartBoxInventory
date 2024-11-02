f0eca89ec659b5b1690fd3640c6ca1de485940d3
Jay <jay@projectchaos.ch>
2024-11-02 16:15:21 +0100

    improved ObjectRecognitionService API

--- code/src/app/shared/services/object-recognition.service.ts
+++ code/src/app/shared/services/object-recognition.service.ts
@@ -16,36 +16,47 @@ export class ObjectRecognitionService {
     });
   }
 
+  private getMobilenet(): Promise<mobilenet.MobileNet> {
+    return mobilenet.load({
+      version: 2,
+      alpha: 1, // Tunable parameter to control accuracy vs speed
+    });
+  }
+
+  /**
+   * Predicts with mobilenet
+   * More classes, more precise but slower
+   * @param source Image or video data to predict
+   * @param num Number of predictions to return
+   * @returns Mobilenet predictions
+   */
   public async predictWithMobilenetModel(
     source: ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement,
+    num: number = 1,
   ): Promise<
     Array<{
       className: string;
       probability: number;
     }>
   > {
-    const model = await mobilenet.load();
-    const predictions = await model.classify(source);
+    const model = await this.getMobilenet();
+    const predictions = await model.classify(source, num);
     return predictions;
   }
 
+  /**
+   * Predicts with coco-ssd
+   * Less classes, less precise but faster
+   * @param source Image or video data to predict
+   * @param num Number of predictions to return
+   * @returns Coco predictions
+   */
   public async predictWithCocoModel(
     source: ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement,
+    num: number = 1,
   ): Promise<cocoSSD.DetectedObject[]> {
     const model = await this.getCocoSSD();
-    const predictions = await model.detect(source);
+    const predictions = await model.detect(source, num);
     return predictions;
-  }
-
-  getTopPrediction(predictions: any) {
-    let maxConfidence = 0;
-    let maxConfidencePrediction = null;
-    predictions.forEach((prediction: any) => {
-      if (prediction.score > maxConfidence) {
-        maxConfidence = prediction.score;
-        maxConfidencePrediction = prediction;
-      }
-    });
-    return maxConfidencePrediction;
   }
 }
