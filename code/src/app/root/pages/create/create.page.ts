fb70856a50086695db720694c352187ef2975b44
Caliv <main@caliv.ch>
2024-11-02 22:31:38 +0100

    Navigation between find and create

--- code/src/app/root/pages/create/create.page.ts
+++ code/src/app/root/pages/create/create.page.ts
@@ -1,21 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component, Signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardAvatar } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 import { CreateItemComponent } from '@root/components/create-item/create-item.component';
 import { ItemComponent } from '@root/components/item/item.component';
+import { RootRoutesEnum } from '@root/root-routes.enum';
 import { ItemModel } from '@root/shared/models/item.model';
 import { StateService } from '@root/state/state.service';
 
 @Component({
   selector: 'root_create-page',
   standalone: true,
-  imports: [CommonModule, CreateItemComponent, ItemComponent],
+  imports: [CommonModule, CreateItemComponent, ItemComponent, MatButtonModule, MatIconModule],
   templateUrl: './create.page.html',
   styleUrl: './create.page.scss',
 })
 export class CreatePage {
   items: Signal<ItemModel[]>;
 
-  constructor(private _state: StateService) {
+  constructor(
+    private _router: Router,
+    private _state: StateService,
+  ) {
     this.items = this._state.getAllItems();
+  }
+
+  findClicked() {
+    this._router.navigate([RootRoutesEnum.Find]);
   }
 }
--- code/src/app/root/pages/find/find.page.ts
+++ code/src/app/root/pages/find/find.page.ts
@@ -1,10 +1,13 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDivider } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
 import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
+import { RootRoutesEnum } from '@root/root-routes.enum';
 import { EntityEnum } from '@root/shared/enums/entity.enum';
 import { BoxModel } from '@root/shared/models/box.model';
 import { ItemModel } from '@root/shared/models/item.model';
@@ -15,46 +18,50 @@ import { concat, merge } from 'rxjs';
   selector: 'root_find-page',
   standalone: true,
   imports: [
-    MatFormField,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatSelect,
+    MatSelectModule,
     FormsModule,
     MatSelectModule,
     ReactiveFormsModule,
-    MatDivider,
+    MatDividerModule,
+    MatButtonModule,
   ],
   templateUrl: './find.page.html',
   styleUrl: './find.page.scss',
 })
 export class FindPage {
-  entities: WritableSignal<(ItemModel|BoxModel)[]>=signal([]);
-  
-  boxesItems:FormControl<EntityEnum[]|null>=new FormControl<EntityEnum[]|null>(null);
-  searchTerm:FormControl<string|null>=new FormControl<string>("");
-  boxList:FormControl<EntityEnum[]|null>=new FormControl<EntityEnum[]|null>(null);
+  entities: WritableSignal<(ItemModel | BoxModel)[]> = signal([]);
+
+  boxesItems: FormControl<EntityEnum[] | null> = new FormControl<EntityEnum[] | null>(null);
+  searchTerm: FormControl<string | null> = new FormControl<string>('');
+  boxList: FormControl<EntityEnum[] | null> = new FormControl<EntityEnum[] | null>(null);
 
   result = new FormControl('');
   boxListResult = new FormControl('');
   searchList: EntityEnum[] = [EntityEnum.Item, EntityEnum.Box];
-  
-  constructor(private _stateService:StateService) {
-    merge(
-    this.boxesItems.valueChanges,
-    this.searchTerm.valueChanges).subscribe(()=>{
-      this.search()
-    })
+
+  constructor(
+    private _router: Router,
+    private _stateService: StateService,
+  ) {
+    merge(this.boxesItems.valueChanges, this.searchTerm.valueChanges).subscribe(() => {
+      this.search();
+    });
   }
 
   search() {
     //console.log("function called");
-    let term:string = this.searchTerm.value??"";
-    let entities:EntityEnum[] = this.boxesItems.value??[];
+    let term: string = this.searchTerm.value ?? '';
+    let entities: EntityEnum[] = this.boxesItems.value ?? [];
     //console.log(term);
-    this._stateService.filter(term,entities)()
-    this.entities.set(this._stateService.filter(term,entities)());
-    console.log(this._stateService.filter(term,entities)());
+    this._stateService.filter(term, entities)();
+    this.entities.set(this._stateService.filter(term, entities)());
+    console.log(this._stateService.filter(term, entities)());
   }
 
+  findClicked() {
+    this._router.navigate([RootRoutesEnum.Create]);
+  }
 }
