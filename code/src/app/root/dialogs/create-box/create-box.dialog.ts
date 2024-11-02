73a085ba3998253593d2884c9f6becc883d32e28
Caliv <main@caliv.ch>
2024-11-02 09:56:50 +0100

    Created Create Box Dialog

---
+++ code/src/app/root/dialogs/create-box/create-box.dialog.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root_create-box-dialog',
+  templateUrl: './create-box.dialog.html',
+  styleUrl: './create-box.dialog.scss',
+})
+export class CreateBoxDialog {
+  constructor() {}
+}
