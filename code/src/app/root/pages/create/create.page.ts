5239d79f479318dac8c7470a4b8344674d8455cf
Caliv <main@caliv.ch>
2024-11-02 21:58:13 +0100

    Improved root_create-item

--- code/src/app/root/pages/create/create.page.ts
+++ code/src/app/root/pages/create/create.page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Signal } from '@angular/core';
+import { CreateItemComponent } from '@root/components/create-item/create-item.component';
+import { ItemComponent } from '@root/components/item/item.component';
+import { ItemModel } from '@root/shared/models/item.model';
+import { StateService } from '@root/state/state.service';
 
 @Component({
   selector: 'root_create-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CreateItemComponent, ItemComponent],
   templateUrl: './create.page.html',
   styleUrl: './create.page.scss',
 })
-export class CreatePage {}
+export class CreatePage {
+  items: Signal<ItemModel[]>;
+
+  constructor(private _state: StateService) {
+    this.items = this._state.getAllItems();
+  }
+}
