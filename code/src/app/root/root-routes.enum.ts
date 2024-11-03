e86c98343b99ea3c7e88d42ae14d46d2a0ea1e0d
Caliv <main@caliv.ch>
2024-11-03 09:08:40 +0100

    Removed test pages

--- code/src/app/root/root-routes.enum.ts
+++ code/src/app/root/root-routes.enum.ts
@@ -2,9 +2,6 @@ export enum RootRoutesEnum {
   Create = 'create',
   Find = 'find',
   Home = 'home',
-  ValiTest = 'vali-test',
-  CupTest = 'cup-test',
-  CalTest = 'cal-test',
   BoxIdParam = 'boxId',
   Box = 'box',
 }
